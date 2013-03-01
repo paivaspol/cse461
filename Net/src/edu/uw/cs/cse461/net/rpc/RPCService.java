f151871ebb4901b8bd5756a4f195c923582e070d
Lee Lee Choo <chooleeelee@gmail.com>
2013-03-01 02:11:14 -0800

    Finished the draft version with a few questions listed as todos

--- Net/src/edu/uw/cs/cse461/net/rpc/RPCService.java
+++ Net/src/edu/uw/cs/cse461/net/rpc/RPCService.java
@@ -1,10 +1,9 @@
 package edu.uw.cs.cse461.net.rpc;
 
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
+import java.io.EOFException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,15 +12,11 @@ import org.json.JSONObject;
 
 import edu.uw.cs.cse461.net.base.NetBase;
 import edu.uw.cs.cse461.net.base.NetLoadable.NetLoadableService;
-import edu.uw.cs.cse461.net.rpc.RPCMessage.RPCCallMessage;
-import edu.uw.cs.cse461.net.rpc.RPCMessage.RPCCallMessage.RPCControlMessage;
-import edu.uw.cs.cse461.net.rpc.RPCMessage.RPCCallMessage.RPCInvokeMessage;
-import edu.uw.cs.cse461.net.rpc.RPCMessage.RPCResponseMessage;
-import edu.uw.cs.cse461.net.rpc.RPCMessage.RPCResponseMessage.RPCErrorResponseMessage;
-import edu.uw.cs.cse461.net.rpc.RPCMessage.RPCResponseMessage.RPCNormalResponseMessage;
 import edu.uw.cs.cse461.net.tcpmessagehandler.TCPMessageHandler;
+import edu.uw.cs.cse461.util.ConfigManager;
 import edu.uw.cs.cse461.util.IPFinder;
 import edu.uw.cs.cse461.util.Log;
+
 
 /**
  * Implements the side of RPC that receives remote invocation requests.
@@ -31,6 +26,14 @@ import edu.uw.cs.cse461.util.Log;
  */
 public class RPCService extends NetLoadableService implements Runnable, RPCServiceInterface {
 	private static final String TAG="RPCService";
+	private int rpcPort;
+	private ServerSocket serverSocket;
+	private HashMap<String, HashMap<String, RPCCallableMethod>> callableMethodStorage;
+	private enum SocketState {
+    	FRESH, PERSISTENT, WAITING, COMPLETED
+	}	
+	private List<TCPMessageHandler> socketList = new ArrayList<TCPMessageHandler>();
+	private List<SocketState> socketStateList = new ArrayList<SocketState>();
 	
 	/**
 	 * Constructor.  Creates the Java ServerSocket and binds it to a port.
@@ -43,6 +46,20 @@ public class RPCService extends NetLoadableService implements Runnable, RPCServi
 	 */
 	public RPCService() throws Exception {
 		super("rpc");
+		callableMethodStorage = new HashMap<String, HashMap<String, RPCCallableMethod>>();
+		ConfigManager config = NetBase.theNetBase().config();
+		rpcPort = config.getAsInt("rpc.server.port", 0);
+		String serverIP = IPFinder.localIP();
+		serverSocket = new ServerSocket();
+		serverSocket.bind(new InetSocketAddress(serverIP, rpcPort));
+		serverSocket.setSoTimeout(NetBase.theNetBase().config().getAsInt("net.timeout.granularity", 500));
+		
+		Thread thread = new Thread() {
+			public void run() {
+				run();
+			}
+		}; 
+		thread.start();
 	}
 	
 	/**
@@ -51,7 +68,137 @@ public class RPCService extends NetLoadableService implements Runnable, RPCServi
 	 */
 	@Override
 	public void run() {
+		// TODO(leelee): RPCCalleeSocket?
+		try {
+			while(!mAmShutdown) {
+				while(true){
+					try {
+						// Add the TCPMessageHandler to the socketList. and also the respective	
+						socketList.add(new TCPMessageHandler(serverSocket.accept()));
+						socketStateList.add(SocketState.FRESH);
+					} catch(SocketTimeoutException e) {
+						// This is normal. Break to continue with receiving messages.
+						break;	
+					}	
+				}
+		
+				// Process connections
+				// Iterate through the socket list to remove closed connection socket.
+				for (int i = 0; i < socketList.size(); i++) {
+					try {
+						TCPMessageHandler tcpSocket = socketList.get(i);
+						while (true) {
+							// Read message as JSONObject.
+							JSONObject message = tcpSocket.readMessageAsJSONObject();
+							String type = message.getString("type");
+							int clientId = message.getInt("id");
+							
+							// TODO(leelee): Double check on what should be returned as a host.
+							
+							if (type.equals("control")) {
+								// Format normal response message that has the id field, host fiels, callid field and also
+								// type field.
+								JSONObject responseMessage = new JSONObject();
+								responseMessage.put("id", i);
+								responseMessage.put("host", "");
+								responseMessage.put("callid", clientId);
+								responseMessage.put("type", "OK");
+								
+								// Check if the caller wants persistent connection
+								if (!message.isNull("option") &&
+										!message.getJSONObject("option").isNull("connection") &&
+										message.getJSONObject("option").getString("connection").equals("keep-alive")) {
+									// Set the state to persistent
+									socketStateList.set(i, SocketState.PERSISTENT);
+									// Add extra key-value in the response message that indicates that we agree with
+									// setting up persistent connection
+									JSONObject connectionJsonObject = new JSONObject();
+									connectionJsonObject.put("connection", "keep-alive");
+									responseMessage.put("value", connectionJsonObject);
+									
+									// TODO(leelee): Must the response id be fixed for every client?
+								}
+								tcpSocket.sendMessage(responseMessage);
+								
+							} else if (type.equals("invoke")) {
+								// Get the method that is being invoked
+								String app = message.getString("app");
+								String method = message.getString("method");
+							
+								// Format response message
+								JSONObject responseMessage = new JSONObject();
+								responseMessage.put("id", i);
+								responseMessage.put("host", "");
+								responseMessage.put("callid", clientId);
+								
+								// Invoke the method if it exists
+								HashMap<String, RPCCallableMethod> map = callableMethodStorage.get(app);
+								JSONObject value = null ;
+								if (map != null) {
+									RPCCallableMethod rpcCallableMethod = map.get(method);
+									if (rpcCallableMethod != null) {
+										value = rpcCallableMethod.handleCall(message.getJSONObject("args"));
+									}
+									responseMessage.put("value", value);
+									responseMessage.put("type", "OK");
+									
+								} else {
+									// No such method, send error message
+									responseMessage.put("message", "some error message");
+									responseMessage.put("type", "ERROR");
+									responseMessage.put("callargs", message);
+								}
+								
+								tcpSocket.sendMessage(responseMessage);
+								
+								// Send response, change the state to complete iff the state is not persistent.
+								if (socketStateList.get(i) != SocketState.PERSISTENT) {
+									socketStateList.set(i, SocketState.COMPLETED);
+								}
+								
+							} else {
+								// TODO(leelee): Not sure if this really needed.
+								throw new Exception("Illegal type message");
+							}
+								
+							// TODO(leelee): can there be a case where caller actually want persistent connection
+							// at the beginning but changed it to not want persistent connection after that
+						}
+					} catch (SocketTimeoutException e) {
+						Log.e(TAG, "Timed out waiting for data on tcp connection");
+					} catch (EOFException e) {
+						// normal termination of loop
+						Log.d(TAG, "EOF on tcpMessageHandlerSocket.readMessageAsString()");
+					} catch (Exception e) {
+						Log.i(TAG, "Unexpected exception while handling connection: " + e.getMessage());
+						socketStateList.set(i, SocketState.COMPLETED);
+					} finally {
+						if ( socketStateList.get(i) == SocketState.COMPLETED) { 
+							try {
+								socketList.get(i).close(); 
+							} catch (Exception e) {
+								// Do nothing.
+							}
+							socketList.remove(i);
+							socketStateList.remove(i);
+							i--;
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			Log.w(TAG, "Server thread exiting due to exception: " + e.getMessage());
+		} finally {
+			if ( serverSocket != null )  {
+				try {
+					serverSocket.close();
+				} catch (Exception e) {
+					// Do nothing.
+				}
+			}
+		}
 	}
+	
 	
 	/**
 	 * Services and applications with RPC callable methods register them with the RPC service using this routine.
@@ -63,6 +210,15 @@ public class RPCService extends NetLoadableService implements Runnable, RPCServi
 	 */
 	@Override
 	public synchronized void registerHandler(String serviceName, String methodName, RPCCallableMethod method) throws Exception {
+		// Put the serviceName, methodName and RPCCallableMethod into the hashmap of hashmap iff the service
+		// name is not a key inside the hashmap yet.
+		HashMap<String, RPCCallableMethod> methodNameToRPCCallableMethodMap = callableMethodStorage.get(serviceName);
+		if (methodNameToRPCCallableMethodMap == null) {
+			methodNameToRPCCallableMethodMap = new HashMap<String, RPCCallableMethod>();
+		}
+		methodNameToRPCCallableMethodMap.put(methodName, method);
+		callableMethodStorage.put(serviceName, methodNameToRPCCallableMethodMap);
+		
 	}
 	
 	/**
@@ -74,7 +230,9 @@ public class RPCService extends NetLoadableService implements Runnable, RPCServi
 	 * @return The existing registration for that method of that service, or null if no registration exists.
 	 */
 	public RPCCallableMethod getRegistrationFor( String serviceName, String methodName) {
-		return null;
+		// Look up in the hashmap of hashmap that we have.
+		HashMap<String, RPCCallableMethod> methodNameToRPCCallableMethodMap = callableMethodStorage.get(serviceName);
+		return methodNameToRPCCallableMethodMap.get(methodName);
 	}
 	
 	/**
@@ -83,11 +241,12 @@ public class RPCService extends NetLoadableService implements Runnable, RPCServi
 	 */
 	@Override
 	public int localPort() {
-		return 0;
+		return rpcPort;
 	}
 	
 	@Override
 	public String dumpState() {
+		// TODO(leelee): I guess I can only work on this when we have the provided solution jar??
 		return "";
 	}
 }
